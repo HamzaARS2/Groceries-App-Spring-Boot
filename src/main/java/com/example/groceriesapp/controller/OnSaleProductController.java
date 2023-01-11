ffe8381088e5290597103f2a99011998a7c62868
HamzaARS2 <hamza1arrastec@gmail.com>
2023-01-11 11:27:37 +0000

    OnSaleProduct Controller init version complete

---
+++ src/main/java/com/example/groceriesapp/controller/OnSaleProductController.java
@@ -0,0 +1,31 @@
+package com.example.groceriesapp.controller;
+
+import com.example.groceriesapp.entity.OnSaleProduct;
+import com.example.groceriesapp.service.OnSaleProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/onsaleproducts")
+public class OnSaleProductController {
+
+    @Autowired
+    private OnSaleProductService service;
+
+    @GetMapping()
+    public List<OnSaleProduct> findAllOnSaleProducts() {
+        return service.getOnSaleProducts();
+    }
+
+    @PostMapping("/publish")
+    public OnSaleProduct saveOnSaleProduct(@RequestBody OnSaleProduct onSaleProduct) {
+        return service.insertOnSaleProduct(onSaleProduct);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteOnSaleProduct(@PathVariable Integer id) {
+        return service.deleteOnSaleProduct(id);
+    }
+}
