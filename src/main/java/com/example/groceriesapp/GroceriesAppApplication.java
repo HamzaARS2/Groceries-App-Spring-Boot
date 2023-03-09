d9ad13555fab1939b5a9fbcd9effea980a8bda2e
HamzaArs2 <hamza1arrastec@gmail.com>
2023-03-09 11:39:43 +0000

    Bug fix

--- src/main/java/com/example/groceriesapp/GroceriesAppApplication.java
+++ src/main/java/com/example/groceriesapp/GroceriesAppApplication.java
@@ -1,8 +1,6 @@
 package com.example.groceriesapp;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
