c5dc395b11e27c1f46d3a5bd7d065ea2548c9cf2
HamzaArs2 <hamza1arrastec@gmail.com>
2023-03-10 18:39:15 +0000

    Fix bug 1

--- src/main/java/com/example/groceriesapp/repository/FavoriteProductRepo.java
+++ src/main/java/com/example/groceriesapp/repository/FavoriteProductRepo.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 public interface FavoriteProductRepo extends JpaRepository<FavoriteProduct, Integer> {
     List<FavoriteProduct> findByCustomerId(String id);
-    @Query("DELETE * FROM favorite_products WHERE customer_id =:customerId AND product_id =:productId")
+    @Query("DELETE * FROM favorite_products p WHERE p.customerId = :customerId AND p.productId = :productId")
     void deleteByCustomerIdAndProductId(String customerId, Integer productId);
 }
